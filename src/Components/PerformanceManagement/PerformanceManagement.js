269f8d33a342dfc89b0401f2acdea68e77ed7a6b
gousiyaprismire <gousiyaprismire@gmail.com>
2025-02-11 17:03:49 +0530

    changes added for Goal Categories Page

--- src/Components/PerformanceManagement/PerformanceManagement.js
+++ src/Components/PerformanceManagement/PerformanceManagement.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./PerformanceManagement.css";
-
+import GoalCategories from "./GoalCategories";
 
 const PerformanceManagement = () => {
   const [showSettings, setShowSettings] = useState(false);
