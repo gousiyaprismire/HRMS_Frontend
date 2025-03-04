78d4a06b72da4859be7ef52f1ca1a2c895666b7e
manjunadhkonjeti3 <manjunadhkonjeti3@gmail.com>
2025-03-04 11:04:08 +0530

    made changes

--- src/Components/AttendanceTime/AttendanceTime.js
+++ src/Components/AttendanceTime/AttendanceTime.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link for routing
-import "./AttendanceTime.css"; // External CSS file
+import { Link } from "react-router-dom";
+import "./AttendanceTime.css";
 
 function AttendanceTime() {
   return (
     <div className="attendanceTime-dashboard">
-      <h1>Attendance & Time Management</h1>
-      <nav>
+      <h1 className="attendanceTime-header">Attendance & Time Management</h1>
+
+      <nav className="attendanceTime-nav">
         <ul>
-          <li><Link to="/attendance-tracking"><button>Employee Attendance Tracking</button></Link></li>
-          <li><Link to="/leave-application"><button>Leave Application</button></Link></li>
-          <li><Link to="/leave-approval"><button>Leave Approval Panel</button></Link></li>
-          <li><Link to="/timesheet"><button>Timesheet Management</button></Link></li>
-          <li><Link to="/holiday"><button>Holiday & Leave Policies</button></Link></li>
+          <li>
+            <Link to="/attendance-tracking">
+              <button className="attendanceTime-button">Employee Attendance Tracking</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/leave-application">
+              <button className="attendanceTime-button">Leave Application</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/leave-approval">
+              <button className="attendanceTime-button">Leave Approval Panel</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/timesheet">
+              <button className="attendanceTime-button">Timesheet Management</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/holiday">
+              <button className="attendanceTime-button">Holiday & Leave Policies</button>
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
